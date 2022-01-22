2a3639ed7825c52d137fe7db0b36426ae4ba716d
Alexsandro Percy <alexsandro.percy@gmail.com>
2022-01-22 18:11:14 -0300

    fix on dettach passenger

--- ju52_utilities.lua
+++ ju52_utilities.lua
@@ -221,11 +221,13 @@ function ju52.dettach_pax(self, player)
     end
 
     -- detach the player
-    player:set_detach()
-    player_api.player_attached[name] = nil
-    player_api.set_animation(player, "stand")
-    player:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
-    --remove_physics_override(player, {speed=1,gravity=1,jump=1})
+    if player then
+        player:set_detach()
+        player_api.player_attached[name] = nil
+        player_api.set_animation(player, "stand")
+        player:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
+        --remove_physics_override(player, {speed=1,gravity=1,jump=1})
+    end
 end
 
 function ju52.checkAttach(self, player)
